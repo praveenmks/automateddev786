f054c6064fdbb837da17333060f3498bac425cff
Praveen <praveen.mukkala@gmail.com>
2015-02-12 16:47:12 +0000

    initial

---
+++ automateddev786/src/main/java/uk/co/o2/jenkins/servletcontroller/TestServlet.java
@@ -0,0 +1,48 @@
+package uk.co.o2.jenkins.servletcontroller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 System.out.println("doGet TestServlet called");
+	}
+
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost TestServlet called");
+		
+		RequestDispatcher rd = request.getRequestDispatcher("test.jsp");
+		
+		rd.forward(request, response);
+	}
+
+}
